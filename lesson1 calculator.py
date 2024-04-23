6d09ea1c73d02de1a1336a0f8c0582e86f6325b6
samia8612 <41755524+samia8612@users.noreply.github.com>
2024-04-23 20:58:42 +0100

    Add files via upload

---
+++ lesson1 calculator.py
@@ -0,0 +1,5 @@
+print("This program is the simple calculator that can multiply 2 numbers together")
+firstNum=int(input("Please enter your first number: "))
+SecondNum=int(input("Please enter your second number: "))
+total = firstNum * SecondNum
+print(f"the product of {firstNum} *{SecondNum} = {total}")
